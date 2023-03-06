5a1a3598bca65da375b571db96ede3948f4e0017
zhangqianxi <2928122014@qq.com>
2023-03-06 15:55:04 +0800

    hot-fix first commit

--- src/main/java/com/zqx/test/GitTest.java
+++ src/main/java/com/zqx/test/GitTest.java
@@ -9,5 +9,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello git!");
         System.out.println("Hello git!!!");
+        System.out.println("Hello git1!!!");
+        System.out.println("Hello git2!!!!");
     }
 }
