c6af28ee8b27be39b5a7282b4ffbe43ace2aa384
zhangqianxi <2928122014@qq.com>
2023-03-06 15:50:35 +0800

    first commit

---
+++ src/main/java/com/zqx/test/GitTest.java
@@ -0,0 +1,12 @@
+package com.zqx.test;
+
+/**
+ * @Author: 张千禧
+ * @CreateTime: 2023-03-06  15:40
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+    }
+}
