4a87abf88eedaec283eb5e37872f58718e8f799d
zhangqianxi <2928122014@qq.com>
2023-03-06 16:18:55 +0800

    push commit

--- src/main/java/com/zqx/test/GitTest.java
+++ src/main/java/com/zqx/test/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("Hello git2!!!!");
         System.out.println("master test!");
         System.out.println("hot-fix test!");
+        System.out.println("push test!");
     }
 }
