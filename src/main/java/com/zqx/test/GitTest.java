136601edf0673c92aa5936b327618253d65b4041
张 <57203607+zqx-usr@users.noreply.github.com>
2023-03-06 16:25:14 +0800

    pull commit

--- src/main/java/com/zqx/test/GitTest.java
+++ src/main/java/com/zqx/test/GitTest.java
@@ -15,5 +15,6 @@ public class GitTest {
         System.out.println("hot-fix test!");
         System.out.println("push test!");
         System.out.println("push2 test!");
+        System.out.println("pull test!");
     }
 }
